d29dacc4b9228c5b8cc8ca33b884c46efd2350ea
GhoraiVanga <ghorai77@gmail.com>
2021-02-15 21:49:54 -0800

    update

---
+++ src/messages/cart.js
@@ -0,0 +1,5 @@
+const cartMessages = {
+  'cart.add.success': 'Add to cart successfully',
+  'cart.add.fail': 'There was an error adding shopping cart',
+};
+export default cartMessages;
---
+++ src/messages/order.js
@@ -0,0 +1,5 @@
+const orderMessages = {
+  'order.add.success': 'Order Success',
+  'order.add.fail': 'There was an error placing an order',
+};
+export default orderMessages;
